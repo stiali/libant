a0e7cd22aab7b1147c9f1e13ac33fe996e378826
huangliling <huangliling@xd.com>
2021-05-17 09:42:53 +0800

    add FilePurger

---
+++ file_utils/file_purger.cpp
@@ -0,0 +1,5 @@
+#include "file_purger.h"
+
+namespace ant {
+
+} // namespace ant
---
+++ file_utils/file_purger.h
@@ -0,0 +1,13 @@
+#ifndef LIBANT_FILE_UTILS_FILE_PURGER_H_
+#define LIBANT_FILE_UTILS_FILE_PURGER_H_
+
+namespace ant {
+
+class FilePurger {
+public:
+private:
+};
+
+} // namespace ant
+
+#endif //LIBANT_FILE_UTILS_FILE_PURGER_H_
