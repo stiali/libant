c817ec927158267e092e1bdd2f72db063a884996
huangliling <huangliling@xd.com>
2021-05-17 11:31:59 +0800

    implementing FilePurger

--- file_utils/file_purger.cpp
+++ file_utils/file_purger.cpp
@@ -1,5 +1,67 @@
+#include <filesystem>
+
+#ifndef _WIN32
+#include <csignal>
+#endif
+
 #include "file_purger.h"
 
+using namespace std;
+namespace fs = std::filesystem;
+
 namespace ant {
+
+void FilePurger::run()
+{
+#ifndef _WIN32
+    // block all signals
+    sigset_t mask;
+    if (sigfillset(&mask) == 0) {
+        pthread_sigmask(SIG_BLOCK, &mask, nullptr);
+    }
+#endif // !_WIN32
+
+    unordered_map<string, PurgingRule> rules;
+    for (;;) {
+        rulesMtx_.lock();
+        rules = rules_;
+        rulesMtx_.unlock();
+
+        time_t tNow = time(nullptr);
+        for (const auto& rule : rules) {
+            purgeOldFiles(tNow, rule.first, rule.second);
+        }
+
+        this_thread::sleep_for(chrono::seconds(300));
+    }
+}
+
+void FilePurger::purgeOldFiles(time_t tNow, const std::string& dir, const PurgingRule& rule)
+{
+    fs::path p(dir);
+
+    error_code ec;
+    auto dirIter = fs::directory_iterator(p, ec);
+    if (ec) {
+        return;
+    }
+
+    for (const auto& entry : dirIter) {
+        time_t lastUpdTime = chrono::duration_cast<chrono::seconds>(fs::last_write_time(entry).time_since_epoch()).count();
+        if (fs::is_regular_file(entry, ec)) {
+            if (tNow - lastUpdTime > rule.inactiveTime_) {
+                fs::remove(entry, ec);
+            }
+        } else if (fs::is_directory(entry, ec)) {
+            if (rule.purgeRecursively_) {
+                purgeOldFiles(tNow, entry.path().string(), rule);
+            }
+
+            if (rule.purgeEmptyDirectory_ && tNow - lastUpdTime > rule.inactiveTime_ && fs::is_empty(entry, ec)) {
+                fs::remove(entry, ec);
+            }
+        }
+    }
+}
 
 } // namespace ant
--- file_utils/file_purger.h
+++ file_utils/file_purger.h
@@ -1,11 +1,105 @@
 #ifndef LIBANT_FILE_UTILS_FILE_PURGER_H_
 #define LIBANT_FILE_UTILS_FILE_PURGER_H_
 
+#include <atomic>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
+
 namespace ant {
 
+/**
+ * Monitor specified directories and purge old files under those directories according to the associated rules.
+ */
 class FilePurger {
 public:
+    /**
+     * Purging rule
+     */
+    class PurgingRule {
+    public:
+        friend class FilePurger;
+
+    public:
+        /**
+         * Construct a purging rule
+         *
+         * @param inactiveTime Purge files whose last modified time are at least `InactiveTime` seconds ago.
+         * @param recursive Whether to purge directory recursively.
+         * @param purgeEmptyDir Whether to purge empty directory.
+         */
+        PurgingRule(time_t inactiveTime, bool recursive = true, bool purgeEmptyDir = true)
+        {
+        }
+
+    private:
+        time_t inactiveTime_;
+        bool purgeRecursively_;
+        bool purgeEmptyDirectory_;
+    };
+
+public:
+    /**
+     * Construct a FilePurger object and start a thread to purge old files.
+     */
+    FilePurger()
+        : stop_(false) // TODO
+        , purgingThread_(&FilePurger::run, this)
+    {
+    }
+
+    ~FilePurger()
+    {
+        stop_ = true; // TODO
+        purgingThread_.join();
+    }
+
+    /**
+     * Add a directory to be monitored and purge old files under this directory according to the associated rule.
+     *
+     * @param dir
+     * @param rule
+     */
+    void Add(std::string dir, const PurgingRule& rule)
+    {
+        rulesMtx_.lock();
+        rules_.insert_or_assign(std::move(dir), rule);
+        rulesMtx_.unlock();
+    }
+
+    /**
+     * Remove a monitored directory
+     *
+     * @param dir
+     */
+    void Remove(const std::string& dir)
+    {
+        rulesMtx_.lock();
+        rules_.erase(dir);
+        rulesMtx_.unlock();
+    }
+
+    /**
+     * Clear all monitored directories
+     */
+    void Clear()
+    {
+        rulesMtx_.lock();
+        rules_.clear();
+        rulesMtx_.unlock();
+    }
+
 private:
+    void run();
+    void purgeOldFiles(time_t tNow, const std::string& dir, const PurgingRule& rule);
+
+private:
+    std::atomic<bool> stop_; // TODO
+    std::thread purgingThread_;
+    std::mutex rulesMtx_;
+    // <dir, purging rule>
+    std::unordered_map<std::string, PurgingRule> rules_;
 };
 
 } // namespace ant
