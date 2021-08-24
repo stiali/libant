07dc67cbe80023454bcce8fc31dbb8152cb35c5f
huangliling <huangliling@xd.com>
2021-08-24 10:43:14 +0800

    feat: add AddChecksum16 and FinishChecksum16

--- include/libant/checksum/checksum.h
+++ include/libant/checksum/checksum.h
@@ -6,14 +6,42 @@
 namespace ant {
 
 /**
+ * Add checksum of `data` to `currentChecksum`. Total data length of the added data should not exceed 120000 bytes.
+ *
+ * @param currentChecksum
+ * @param data
+ * @param dataLen
+ * @return checksum of `data` plus `currentChecksum`
+ */
+uint32_t AddChecksum16(uint32_t currentChecksum, const void* data, size_t dataLen);
+
+/**
+ * fold the 32-bit checksum into a 16-bit checksum
+ *
+ * @param currentChecksum
+ * @return the folded 16-bit checksum
+ */
+inline uint16_t FinishChecksum16(uint32_t currentChecksum)
+{
+    // fold the 32-bit checksum into a 16-bit checksum
+    while (currentChecksum > 0xFFFF) {
+        currentChecksum = (currentChecksum >> 16) + (currentChecksum & 0xFFFF);
+    }
+    return static_cast<uint16_t>(currentChecksum);
+}
+
+/**
  * Checksum16 computes a 16-bit checksum of `data`
  *
  * @param data
- * @param dataLen cannot be longer then 60000 bytes
+ * @param dataLen cannot be longer then 120000 bytes
  *
  * @return a 16-bit checksum of `data`
  */
-uint16_t Checksum16(const void* data, size_t dataLen);
+inline uint16_t Checksum16(const void* data, size_t dataLen)
+{
+    return FinishChecksum16(AddChecksum16(0, data, dataLen));
+}
 
 } // namespace ant
 
--- src/checksum/checksum.cpp
+++ src/checksum/checksum.cpp
@@ -3,35 +3,18 @@
 
 namespace ant {
 
-/**
- * Checksum16 computes a 16-bit checksum of `data`
- *
- * @param data
- * @param dataLen cannot be longer then 60000 bytes
- *
- * @return a 16-bit checksum of `data`
- */
-uint16_t Checksum16(const void* data, size_t dataLen)
+uint32_t AddChecksum16(uint32_t currentChecksum, const void* data, size_t dataLen)
 {
-    assert(dataLen <= 60000);
-
-    uint32_t checksum = 0;
-
     // add data altogether
     auto data16 = reinterpret_cast<const uint16_t*>(data);
     while (dataLen > 1) {
-        checksum += *(data16++);
+        currentChecksum += *(data16++);
         dataLen -= 2;
     }
     if (dataLen) {
-        checksum += *reinterpret_cast<const uint8_t*>(data16);
+        currentChecksum += *reinterpret_cast<const uint8_t*>(data16);
     }
-    // fold the 32-bit checksum into a 16-bit checksum
-    while (checksum > 0xFFFF) {
-        checksum = (checksum >> 16) + (checksum & 0xFFFF);
-    }
-
-    return static_cast<uint16_t>(checksum);
+    return currentChecksum;
 }
 
 } // namespace ant
