b5c56636f160300ea5fb4bd29bc403cb2b0b359d
huangliling <huangliling@xd.com>
2021-10-28 09:55:29 +0800

    feat: add likely.h

---
+++ include/libant/utils/likely.h
@@ -0,0 +1,16 @@
+#ifndef LIBANT_INCLUDE_LIBANT_UTILS_LIKELY_H_
+#define LIBANT_INCLUDE_LIBANT_UTILS_LIKELY_H_
+
+#ifndef _WIN32
+
+#define likely(cond_) __builtin_expect(!!(cond_), 1)
+#define unlikely(cond_) __builtin_expect(!!(cond_), 0)
+
+#else
+
+#define likely(cond_)
+#define unlikely(cond_)
+
+#endif
+
+#endif //LIBANT_INCLUDE_LIBANT_UTILS_LIKELY_H_
