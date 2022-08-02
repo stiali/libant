be207502c2b99d6d33240c6d9528d7a6e79e7372
huangliling <andy@xd.com>
2022-08-02 14:52:46 +0800

    add LIBANT_NO_UNALIGNED_ACCESS to check if unaligned memory access is allowed

--- include/libant/utils/os.h
+++ include/libant/utils/os.h
@@ -27,4 +27,12 @@
 
 #endif
 
+#if defined(i386) || defined(__i386) || defined(__i386__) || defined(__386) || defined(__IA32__) || defined(_M_IX86) || defined(_X86_) || defined(__X86__) || \
+    defined(__I86__) || defined(__INTEL__) || defined(__THW_INTEL__) || defined(__x86_64) || defined(__x86_64__) || defined(__amd64) || defined(__amd64__) || \
+    defined(_M_X64) || defined(_M_AMD64) || defined(__itanium__) || defined(__ia64) || defined(__ia64__) || defined(_IA64) || defined(__IA64__) ||            \
+    defined(_M_IA64) || defined(__aarch64__)
+#else
+#define LIBANT_NO_UNALIGNED_ACCESS
+#endif
+
 #endif //LIBANT_INCLUDE_LIBANT_UTILS_OS_H_
