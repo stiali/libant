bea5957d60f96194553fbc0774b3e71c5cd10316
huangliling <andy@xd.com>
2022-08-03 15:44:48 +0800

    add ToUnixTime

--- include/libant/utils/time_utils.h
+++ include/libant/utils/time_utils.h
@@ -1,6 +1,7 @@
 #ifndef LIBANT_INCLUDE_LIBANT_TIME_UTILS_TIME_UTILS_H_
 #define LIBANT_INCLUDE_LIBANT_TIME_UTILS_TIME_UTILS_H_
 
+#include <ctime>
 #include <chrono>
 
 namespace ant {
@@ -15,6 +16,19 @@ template<typename Duration>
 time_t ToTimestamp(const std::chrono::time_point<std::chrono::system_clock>& t)
 {
     return std::chrono::duration_cast<Duration>(t.time_since_epoch()).count();
+}
+
+inline time_t ToUnixTime(int year, int mon, int day, int hour, int min, int sec)
+{
+    tm tm;
+    tm.tm_isdst = 0;
+    tm.tm_year = year - 1900;
+    tm.tm_mon = mon - 1;
+    tm.tm_mday = day;
+    tm.tm_hour = hour;
+    tm.tm_min = min;
+    tm.tm_sec = sec;
+    return mktime(&tm);
 }
 
 } // namespace ant
