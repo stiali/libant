e6e8fb9adbaf48ebcb351daf86a402698181450f
huangliling <andy@xd.com>
2022-08-03 15:11:59 +0800

    fix compilation warning under Windows

--- include/libant/net/detail/tcp.h
+++ include/libant/net/detail/tcp.h
@@ -4,7 +4,12 @@
 #ifndef _WIN32
 #include <netinet/tcp.h>
 #else
+#pragma warning(push)
+#pragma warning(disable: 4201)
+
 #include "win/tcp.h"
+
+#pragma warning(pop)
 #endif
 
 #endif //LIBANT_INCLUDE_LIBANT_NET_DETAIL_TCP_H_
