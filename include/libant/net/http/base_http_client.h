e8a5a95a080fd03f8364dd9b7ba21f848437a74e
huangliling <andy@xd.com>
2022-08-04 16:20:39 +0800

    add BaseHttpClient

---
+++ include/libant/net/http/base_http_client.h
@@ -0,0 +1,181 @@
+#ifndef LIBANT_INCLUDE_LIBANT_NET_HTTP_BASE_HTTP_CLIENT_H_
+#define LIBANT_INCLUDE_LIBANT_NET_HTTP_BASE_HTTP_CLIENT_H_
+
+#include <deque>
+#include <unordered_map>
+#include <boost/asio/ssl/context.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+
+namespace ant {
+
+class base_http_client : public std::enable_shared_from_this<base_http_client> {
+public:
+    using callback = std::function<void(boost::beast::error_code, boost::beast::http::response<boost::beast::http::string_body>)>;
+
+public:
+    base_http_client(boost::asio::io_context& ioCtx, const std::string& svrAddr, uint16_t maxRetryTimes = 2,
+                     std::chrono::milliseconds ioTimeoutMs = std::chrono::milliseconds(5000),
+                     std::chrono::milliseconds connectTimeoutMs = std::chrono::milliseconds(2000), const char* defPort = "80");
+
+    virtual ~base_http_client()
+    {
+        boost::beast::error_code ec(boost::beast::errc::operation_canceled, boost::beast::generic_category());
+        for (auto p : pendingRequests_) {
+            if (p->cb) {
+                p->cb(ec, response_);
+            }
+            delete p;
+        }
+    }
+
+    void get(callback cb, const std::string& target, const std::unordered_map<std::string, std::string>* params = nullptr,
+             const std::vector<std::string>* cookies = nullptr)
+    {
+        request(std::move(cb), boost::beast::http::verb::get, target, params, nullptr, nullptr, cookies, nullptr);
+    }
+
+    void post(callback cb, const std::string& target, const std::unordered_map<std::string, std::string>* params = nullptr,
+              const std::string* contentType = nullptr, const std::string* httpBody = nullptr, const std::vector<std::string>* cookies = nullptr,
+              const std::unordered_map<boost::beast::http::field, std::string>* additionalHeaders = nullptr)
+    {
+        request(std::move(cb), boost::beast::http::verb::post, target, params, contentType, httpBody, cookies, additionalHeaders);
+    }
+
+protected:
+    void on_handshake(const boost::beast::error_code& ec);
+    void on_request_sent(const boost::beast::error_code& ec, size_t);
+    void on_response_read(const boost::beast::error_code& ec, size_t);
+
+private:
+    virtual boost::beast::tcp_stream& get_stream() = 0;
+    virtual void reset_stream() = 0;
+    virtual void handshake() = 0;
+    virtual void send_request() = 0;
+    virtual void read_response() = 0;
+
+private:
+    struct request_params {
+        request_params(callback cb_func)
+            : cb(std::move(cb_func))
+        {
+        }
+
+        request_params(callback cb_func, boost::beast::http::verb http_method, const std::string& http_target,
+                       const std::unordered_map<std::string, std::string>* params, const std::string* http_content_type, const std::string* http_body,
+                       const std::vector<std::string>* http_cookies, const std::unordered_map<boost::beast::http::field, std::string>* additionalHeaders);
+
+        ~request_params()
+        {
+            delete content_type;
+            delete body;
+            delete cookies;
+            delete additional_headers;
+        }
+
+        callback cb;
+        boost::beast::http::verb method;
+        std::string target;
+        const std::string* content_type{nullptr};
+        std::string* body{nullptr};
+        const std::string* cookies{nullptr};
+        std::unordered_map<boost::beast::http::field, std::string>* additional_headers{nullptr};
+    };
+
+private:
+    void request(callback cb, boost::beast::http::verb method, const std::string& target, const std::unordered_map<std::string, std::string>* params,
+                 const std::string* contentType, const std::string* httpBody, const std::vector<std::string>* cookies,
+                 const std::unordered_map<boost::beast::http::field, std::string>* additionalHeaders)
+    {
+        if (!pendingRequests_.empty()) {
+            pendingRequests_.push_back(new request_params(std::move(cb), method, target, params, contentType, httpBody, cookies, additionalHeaders));
+            return;
+        }
+
+        pendingRequests_.push_back(new request_params(std::move(cb)));
+        do_request(method, target, params, contentType, httpBody, cookies, additionalHeaders);
+    }
+
+    void response(const boost::beast::error_code& ec)
+    {
+        auto req = pendingRequests_.front();
+        if (req->cb) {
+            req->cb(ec, std::move(response_));
+        } else {
+            response_.clear();
+            response_.body().clear();
+        }
+        pendingRequests_.pop_front();
+        delete req;
+
+        if (!pendingRequests_.empty()) {
+            do_request(pendingRequests_.front());
+        }
+    }
+
+    void do_request(boost::beast::http::verb method, const std::string& target, const std::unordered_map<std::string, std::string>* params,
+                    const std::string* contentType, const std::string* httpBody, const std::vector<std::string>* cookies,
+                    const std::unordered_map<boost::beast::http::field, std::string>* additionalHeaders)
+    {
+        retriedTimes_ = 0;
+        init_request(method, target, params, contentType, httpBody, cookies, additionalHeaders);
+        if (connected_) {
+            return send_request();
+        }
+        resolve();
+    }
+
+    void do_request(const request_params* req_params)
+    {
+        retriedTimes_ = 0;
+        init_request(req_params);
+        if (connected_) {
+            return send_request();
+        }
+        resolve();
+    }
+
+    void init_request(boost::beast::http::verb method, const std::string& target, const std::unordered_map<std::string, std::string>* params,
+                      const std::string* contentType, const std::string* httpBody, const std::vector<std::string>* cookies,
+                      const std::unordered_map<boost::beast::http::field, std::string>* additionalHeaders);
+    void init_request(const request_params* req_params);
+
+    void resolve()
+    {
+        // Look up the domain name
+        resolver_.async_resolve(host_, port_, boost::beast::bind_front_handler(&base_http_client::on_resolve, shared_from_this()));
+    }
+    void on_resolve(const boost::beast::error_code& ec, boost::asio::ip::tcp::resolver::results_type results);
+
+    void connect()
+    {
+        auto& stream = get_stream();
+        // Set a timeout on the operation
+        stream.expires_after(connectTimeout_);
+        // Make the connection on the IP address we get from a lookup
+        stream.async_connect(resolvedResults_, boost::beast::bind_front_handler(&base_http_client::on_connect, shared_from_this()));
+    }
+    void on_connect(const boost::beast::error_code& ec, const boost::asio::ip::tcp::resolver::results_type::endpoint_type&);
+
+protected:
+    const std::chrono::milliseconds ioTimeout_;
+    std::string host_;
+    std::string port_;
+    boost::beast::flat_buffer buffer_;
+    boost::beast::http::request<boost::beast::http::string_body> request_;
+    boost::beast::http::response<boost::beast::http::string_body> response_;
+
+private:
+    const std::chrono::milliseconds connectTimeout_;
+    const uint16_t maxRetryTimes_;
+    uint16_t retriedTimes_;
+    bool connected_;
+    std::string tmpStr_;
+    boost::asio::ip::tcp::resolver resolver_;
+    boost::asio::ip::tcp::resolver::results_type resolvedResults_;
+    std::deque<request_params*> pendingRequests_;
+};
+
+} // namespace ant
+
+#endif //LIBANT_INCLUDE_LIBANT_NET_HTTP_BASE_HTTP_CLIENT_H_
---
+++ src/net/http/base_http_client.cpp
@@ -0,0 +1,266 @@
+#include <boost/algorithm/string.hpp>
+#include <boost/beast/version.hpp>
+#include <libant/net/http/base_http_client.h>
+
+using namespace std;
+
+namespace ant {
+
+//================================================================================
+// Public Methods
+//================================================================================
+
+base_http_client::base_http_client(boost::asio::io_context& ioCtx, const std::string& svrAddr, uint16_t maxRetryTimes, std::chrono::milliseconds ioTimeoutMs,
+                                   std::chrono::milliseconds connectTimeoutMs, const char* defPort)
+    : ioTimeout_(ioTimeoutMs)
+    , connectTimeout_(connectTimeoutMs)
+    , maxRetryTimes_(maxRetryTimes)
+    , retriedTimes_(0)
+    , connected_(false)
+    , resolver_(ioCtx)
+{
+    vector<string> hostPort;
+    boost::split(hostPort, svrAddr, boost::is_any_of(":"));
+    host_ = move(hostPort[0]);
+    if (hostPort.size() >= 2) {
+        port_ = move(hostPort[1]);
+    } else {
+        port_ = defPort;
+    }
+}
+
+//================================================================================
+// Protected Methods
+//================================================================================
+
+void base_http_client::on_handshake(const boost::beast::error_code& ec)
+{
+    if (!ec) {
+        connected_ = true;
+        return send_request();
+    }
+
+    reset_stream();
+    if (ec.value() != boost::beast::errc::operation_canceled && retriedTimes_ < maxRetryTimes_) {
+        ++retriedTimes_;
+        return resolve();
+    }
+
+    response(ec);
+}
+
+void base_http_client::on_request_sent(const boost::beast::error_code& ec, size_t)
+{
+    if (!ec) {
+        return read_response();
+    }
+
+    connected_ = false;
+    reset_stream();
+    if (ec.value() != boost::beast::errc::operation_canceled && retriedTimes_ < maxRetryTimes_) {
+        ++retriedTimes_;
+        return resolve();
+    }
+
+    response(ec);
+}
+
+void base_http_client::on_response_read(const boost::beast::error_code& ec, size_t)
+{
+    if (!ec) {
+        if (!response_.keep_alive()) {
+            connected_ = false;
+            buffer_.clear();
+            reset_stream();
+        }
+
+        if (response_.result() == boost::beast::http::status::ok || retriedTimes_ >= maxRetryTimes_) {
+            return response(ec);
+        }
+
+        response_.clear();
+        response_.body().clear();
+        ++retriedTimes_;
+        if (connected_) {
+            return send_request();
+        }
+        return resolve();
+    }
+
+    connected_ = false;
+    buffer_.clear();
+    reset_stream();
+    if (ec.value() != boost::beast::errc::operation_canceled && retriedTimes_ < maxRetryTimes_) {
+        response_.clear();
+        response_.body().clear();
+        ++retriedTimes_;
+        return resolve();
+    }
+
+    response(ec);
+}
+
+//================================================================================
+// Private Methods
+//================================================================================
+
+void base_http_client::init_request(boost::beast::http::verb method, const std::string& target, const std::unordered_map<std::string, std::string>* params,
+                                    const std::string* contentType, const std::string* httpBody, const std::vector<std::string>* cookies,
+                                    const std::unordered_map<boost::beast::http::field, std::string>* additionalHeaders)
+{
+    request_.clear();
+    request_.version(11);
+    request_.set(boost::beast::http::field::host, host_);
+    request_.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    request_.method(method);
+
+    tmpStr_.clear();
+    tmpStr_.append(target);
+    if (params) {
+        tmpStr_.append("?");
+        for (const auto& kv : *params) {
+            tmpStr_.append(kv.first).append("=").append(kv.second).append("&");
+        }
+        tmpStr_.resize(tmpStr_.size() - 1);
+    }
+    request_.target(tmpStr_);
+
+    if (contentType) {
+        request_.set(boost::beast::http::field::content_type, *contentType);
+    }
+
+    if (cookies) {
+        tmpStr_.clear();
+        tmpStr_.reserve(512);
+        for (const auto& ck : *cookies) {
+            tmpStr_.append(ck).append("; ");
+        }
+        if (tmpStr_.size() >= 2) {
+            tmpStr_.resize(tmpStr_.size() - 2);
+        }
+        request_.set(boost::beast::http::field::cookie, tmpStr_);
+    }
+
+    if (additionalHeaders) {
+        for (const auto& kv : *additionalHeaders) {
+            request_.set(kv.first, kv.second);
+        }
+    }
+
+    // 这个必须放在最后
+    if (httpBody) {
+        request_.body() = *httpBody;
+    } else {
+        request_.body().clear();
+    }
+    request_.prepare_payload();
+}
+
+void base_http_client::init_request(const request_params* req_params)
+{
+    request_.clear();
+    request_.version(11);
+    request_.set(boost::beast::http::field::host, host_);
+    request_.set(boost::beast::http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+    request_.method(req_params->method);
+    request_.target(req_params->target);
+
+    if (req_params->content_type) {
+        request_.set(boost::beast::http::field::content_type, *req_params->content_type);
+    }
+
+    if (req_params->cookies) {
+        request_.set(boost::beast::http::field::cookie, *req_params->cookies);
+    }
+
+    if (req_params->additional_headers) {
+        for (const auto& kv : *req_params->additional_headers) {
+            request_.set(kv.first, kv.second);
+        }
+    }
+
+    // 这个必须放在最后
+    if (req_params->body) {
+        request_.body() = move(*req_params->body);
+    } else {
+        request_.body().clear();
+    }
+    request_.prepare_payload();
+}
+
+void base_http_client::on_resolve(const boost::beast::error_code& ec, boost::asio::ip::tcp::resolver::results_type results)
+{
+    if (!ec) {
+        resolvedResults_ = move(results);
+        return connect();
+    }
+
+    if (ec.value() != boost::beast::errc::operation_canceled && retriedTimes_ < maxRetryTimes_) {
+        ++retriedTimes_;
+        return resolve();
+    }
+
+    response(ec);
+}
+
+void base_http_client::on_connect(const boost::beast::error_code& ec, const boost::asio::ip::tcp::resolver::results_type::endpoint_type&)
+{
+    if (!ec) {
+        return handshake();
+    }
+
+    if (ec.value() != boost::beast::errc::operation_canceled && retriedTimes_ < maxRetryTimes_) {
+        ++retriedTimes_;
+        return resolve();
+    }
+
+    response(ec);
+}
+
+//================================================================================
+// request_params public Methods
+//================================================================================
+
+base_http_client::request_params::request_params(callback cb_func, boost::beast::http::verb http_method, const std::string& http_target,
+                                                 const std::unordered_map<std::string, std::string>* params, const std::string* http_content_type,
+                                                 const std::string* http_body, const std::vector<std::string>* http_cookies,
+                                                 const std::unordered_map<boost::beast::http::field, std::string>* additionalHeaders)
+    : cb(std::move(cb_func))
+    , method(http_method)
+    , target(http_target)
+{
+    if (params) {
+        target.append("?");
+        for (const auto& kv : *params) {
+            target.append(kv.first).append("=").append(kv.second).append("&");
+        }
+        target.resize(target.size() - 1);
+    }
+
+    if (http_content_type) {
+        content_type = new std::string(*http_content_type);
+    }
+
+    if (http_body) {
+        body = new std::string(*http_body);
+    }
+
+    if (http_cookies) {
+        auto cks = new std::string();
+        cks->reserve(512);
+        for (const auto& ck : *http_cookies) {
+            cks->append(ck).append("; ");
+        }
+        if (cks->size() >= 2) {
+            cks->resize(cks->size() - 2);
+        }
+        cookies = cks;
+    }
+
+    if (additionalHeaders) {
+        additional_headers = new std::unordered_map<boost::beast::http::field, std::string>;
+        *additional_headers = *additionalHeaders;
+    }
+}
+
+} // namespace ant
